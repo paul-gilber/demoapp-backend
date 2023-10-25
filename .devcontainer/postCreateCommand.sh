f690e696d51587bc1c6e9a370d3fe2a5e3a9859c
Paul Gilber <gilber.paul.s@gmail.com>
2023-10-25 12:46:06 +0000

    install yamllint, yq, jq, add yamllint config

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,6 @@
 # Install MySQL after container creation.
 # Failed feature: ghcr.io/devcontainers-contrib/features/mysql-homebrew:1
 brew install mysql
+
+# No available feature for installing Container Structure Test https://github.com/GoogleContainerTools/container-structure-test
+brew install container-structure-test
